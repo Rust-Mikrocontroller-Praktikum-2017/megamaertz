2c53e16e256a8a1a4bd4a5dec692834f9041f845
JanKeim <jan.keim@protonmail.com>
2017-04-06 10:13:44 +0200

    fix issue of rendering cursor at small positions
    There was an issue of panics when small positions where calculated and could overflow into "negative" numbers.
    Fixed by enforce wrapping_add and _sub to purposely overflow (they will be ignored either way at the range check).

--- src/renderer.rs
+++ src/renderer.rs
@@ -5,59 +5,47 @@ const DISPLAY_HEIGHT: u16 = 272;
 
 
 pub struct Renderer<'a> {
-    display: &'a mut lcd::Lcd
+    display: &'a mut lcd::Lcd,
+    last_touch: (u16, u16),
 }
 
 impl<'a> Renderer<'a> {
     pub fn new(display: &'a mut lcd::Lcd) -> Self {
-        Renderer{
+        Renderer {
             display: display,
+            last_touch: (240, 136),
         }
     }
 
     fn coord_is_inside(x: u16, y: u16) -> bool {
-        x > 0 && x < DISPLAY_HEIGHT && y > 0 && y < DISPLAY_WIDTH
+        x < DISPLAY_WIDTH && y < DISPLAY_HEIGHT
     }
 
     pub fn render_pixel(&mut self, x: u16, y: u16, color: u16) {
-        if Self::coord_is_inside(x,y) {
+        if Self::coord_is_inside(x, y) {
             self.display.print_point_color_at(x, y, color);
-        };
-    }
-
-    pub fn cursor(&mut self, x:u16, y:u16) {
-        for i in 0..13 {
-            self.render_pixel(x + i, y, 0xFFFF);
-            self.render_pixel(x - i, y, 0xFFFF);
-            self.render_pixel(x, y + i, 0xFFFF);
-            self.render_pixel(x, y - i, 0xFFFF);
         }
-
     }
 
-    pub fn draw_colorful_square(&mut self) {
-        let colors = [0xFF00, 0xAF00];
-        let start = (240, 136);
-        let border_width = 4;
+    pub fn cursor(&mut self, x: u16, y: u16) {
+        self.remove_last_cursor();
+        for i in 0..13 {
+            self.render_pixel(x.wrapping_add(i), y, 0xFFFF);
+            self.render_pixel(x.wrapping_sub(i), y, 0xFFFF);
+            self.render_pixel(x, y.wrapping_add(i), 0xFFFF);
+            self.render_pixel(x, y.wrapping_sub(i), 0xFFFF);
+        }
+        self.last_touch = (x, y);
+    }
 
-        for n in 0..20 {
-            let color = colors[(n as usize) % 2];
-            let radius = n * border_width;
-            for j in 0..border_width {
-                for i in 0..radius + 1 + j {
-                    // top and bottom
-                    self.render_pixel(start.0 - i, start.1 - radius - j, color);
-                    self.render_pixel(start.0 + i, start.1 - radius - j, color);
-                    self.render_pixel(start.0 - i, start.1 + radius + j, color);
-                    self.render_pixel(start.0 + i, start.1 + radius + j, color);
-
-                    // left and right
-                    self.render_pixel(start.0 - radius - j, start.1 - i, color);
-                    self.render_pixel(start.0 - radius - j, start.1 + i, color);
-                    self.render_pixel(start.0 + radius + j, start.1 - i, color);
-                    self.render_pixel(start.0 + radius + j, start.1 + i, color);
-                }
-            }
+    fn remove_last_cursor(&mut self) {
+        let x = self.last_touch.0;
+        let y = self.last_touch.1;
+        for i in 0..13 {
+            self.render_pixel(x.wrapping_add(i), y, 0x0000);
+            self.render_pixel(x.wrapping_sub(i), y, 0x0000);
+            self.render_pixel(x, y.wrapping_add(i), 0x0000);
+            self.render_pixel(x, y.wrapping_sub(i), 0x0000);
         }
     }
 }
