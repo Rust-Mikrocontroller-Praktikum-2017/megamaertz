9efede728e10a9a878723aa91e53a9f912923560
Vanessa Steurer <vanessa_steurer@web.de>
2017-04-12 10:56:06 +0200

    Constants for Game Mode Button and Mic.

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,8 @@ pub const DISPLAY_SIZE: (u16, u16) = (480, 272);
 pub const GAME_OVER_SIZE: (u16, u16) = (480, 64);
 pub const GAME_OVER_OFFSET_Y: u16 = 90;
 pub const START_SIZE: (u16, u16) = (480, 74);
-pub const SILENT_BTN_SIZE: (u16, u16) = (50, 50);
+pub const SILENT_BTN_SIZE: (u16, u16) = (40, 40);
+pub const GAME_MODE_BTN_SIZE: (u16, u16) = (80, 80);
 
 // targets
 pub const TARGET_SIZE_50: (u16, u16) = (50, 50);
